99c3d76a8d204bb0e110c4e652009d30ef3b6c85
2025755-coder <2025755@student.cct.ie>
2026-05-18 21:33:06 +0100

    Update CA_2.java

--- src/main/java/CA_2/CA_2.java
+++ src/main/java/CA_2/CA_2.java
@@ -8,9 +8,35 @@ package CA_2;
  *
  * @author tina
  */
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class CA_2 {
 
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final List<Employee> EMPLOYEES = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("CA_2 Organisation Prototype");
+        System.out.println("----------------------------------------");
+
+        loadInitialData();
+
+        boolean running = true;
+        while (running) {
+            printMenu();
+            MenuOption option = readMenuOption();
+
+            switch (option) {
+                case SORT -> sortAndDisplayEmployees();
+                case SEARCH -> searchEmployeeByName();
+                case ADD_RECORDS -> addEmployeeRecord();
+                case CREATE_BINARY_TREE -> buildAndShowHierarchyTree();
+                case EXIT -> running = false;
+            }
+        }
+
+        System.out.println("Program ended.");
     }
-}
